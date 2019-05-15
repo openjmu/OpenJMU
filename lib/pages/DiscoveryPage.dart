77b1c9f2d853addc366239ec5777538e15b5cfe1
Evsio0n <1344729087@qq.com>
2019-05-15 14:40:36 +0800

    Add png Files

--- lib/pages/DiscoveryPage.dart
+++ lib/pages/DiscoveryPage.dart
@@ -82,8 +82,8 @@ class DiscoveryPageState extends State<DiscoveryPage> {
                 children: <Widget>[
                     RichText(text: TextSpan(
                         children: <TextSpan>[
-                            TextSpan(text: "${DateFormat("MMM dd日，", "zh_CN").format(now)}"),
-                            if (currentWeek != null) TextSpan(text: "第$currentWeek周，"),
+                            TextSpan(text: "${DateFormat("MMMdd日,","zh_CN").format(now)}"),
+                            if (currentWeek != null) TextSpan(text: "第$currentWeek周,"),
                             TextSpan(text: "${DateFormat("EEEE", "zh_CN").format(now)}"),
                         ],
                         style: TextStyle(fontSize: 28, color: Theme.of(context).textTheme.caption.color),
