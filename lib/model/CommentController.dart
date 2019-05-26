adc8facf94cc5b8c10583e9d05b7bfa5cefc32b9
AlexVincent525 <github@alexv525.com>
2019-05-27 00:48:19 +0800

    Change can load more condition for controller.

--- lib/model/CommentController.dart
+++ lib/model/CommentController.dart
@@ -193,7 +193,7 @@ class _CommentListState extends State<CommentList> with AutomaticKeepAliveClient
             );
             List<Comment> commentList = [];
             List _topics = jsonDecode(result)['replylist'];
-            var _total = jsonDecode(result)['total'];
+            var _total = jsonDecode(result)['total'], _count = jsonDecode(result)['count'];
             if (_total is String) _total = int.parse(_total);
             for (var commentData in _topics) {
                 commentList.add(CommentAPI.createComment(commentData['reply']));
@@ -206,7 +206,7 @@ class _CommentListState extends State<CommentList> with AutomaticKeepAliveClient
                     _showLoading = false;
                     _firstLoadComplete = true;
                     _isLoading = false;
-                    _canLoadMore = _idList.length < _total;
+                    _canLoadMore = _idList.length < _total && (_count != 0 && _count != "0");
                     _lastValue = _idList.isEmpty ? 0 : widget._commentController.lastValue(_idList.last);
                 });
             }
@@ -229,7 +229,7 @@ class _CommentListState extends State<CommentList> with AutomaticKeepAliveClient
             List<Comment> commentList = [];
             List<int> idList = [];
             List _topics = jsonDecode(result)['replylist'];
-            var _total = jsonDecode(result)['total'];
+            var _total = jsonDecode(result)['total'], _count = jsonDecode(result)['count'];
             if (_total is String) _total = int.parse(_total);
             for (var commentData in _topics) {
                 commentList.add(CommentAPI.createComment(commentData['reply']));
@@ -243,7 +243,7 @@ class _CommentListState extends State<CommentList> with AutomaticKeepAliveClient
                     _showLoading = false;
                     _firstLoadComplete = true;
                     _isLoading = false;
-                    _canLoadMore = _idList.length < _total;
+                    _canLoadMore = _idList.length < _total && (_count != 0 && _count != "0");
                     _lastValue = _idList.isEmpty ? 0 : widget._commentController.lastValue(_idList.last);
 
                 });
