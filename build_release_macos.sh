9d4918ededaad0518498940d1490afc97a35079e
alexv525 <github@alexv525.com>
2020-10-06 23:22:30 +0800

    🍏  Enhance build scripts for iOS archive.

--- build_release_macos.sh
+++ build_release_macos.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+
+# Define current time
+curtime=$(date +%Y-%m-%d\ %H-%M-%S)
+
+# Clean first
 flutter clean
+
+# Build iOS Runner.app
 flutter build ios --release
 cd build/ios/iphoneos/Runner.app/Frameworks
 cd App.framework
@@ -8,5 +15,20 @@ cd ..
 cd Flutter.framework
 xcrun bitcode_strip -r Flutter -o Flutter
 cd ../../../../../../
+
+# Archive Runner.app
+xcodebuild -workspace ios/Runner.xcworkspace \
+  -scheme Runner clean archive -configuration release \
+  -sdk iphoneos -archivePath build/ios/Runner.xcarchive \
+  -quiet
+
+# Export IPA
+xcodebuild -exportArchive -archivePath release/Runner.xcarchive \
+  -exportOptionsPlist ExportOptions.plist \
+  -exportPath release/"Runner $curtime" \
+  -quiet
+
+# Build Android APK
 flutter build apk --release
+
 exit 0
