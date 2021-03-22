62e10406aafa542ae5f5773fd9a50003cd805952
alexv525 <github@alexv525.com>
2021-03-22 14:02:17 +0800

    💄 Improve material wrapping for the error widget

--- lib/main.dart
+++ lib/main.dart
@@ -331,69 +331,72 @@ void _rebuildAllChildren(BuildContext context) {
 
 void _customizeErrorWidget() {
   ErrorWidget.builder = (FlutterErrorDetails details) {
-    return Container(
-      margin: EdgeInsets.all(16.w),
-      padding: EdgeInsets.all(30.w),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(13.w),
-        color: currentTheme.accentColor.withOpacity(0.125),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          SvgPicture.asset(
-            R.ASSETS_PLACEHOLDERS_NO_NETWORK_SVG,
-            width: 50.w,
-            color: currentTheme.iconTheme.color,
-          ),
-          VGap(20.w),
-          Text(
-            '出现了不可预料的错误 (>_<)',
-            style: TextStyle(
-              color: currentTheme.textTheme.caption.color,
-              fontSize: 22.sp,
+    return Material(
+      type: MaterialType.transparency,
+      child: Container(
+        margin: EdgeInsets.all(16.w),
+        padding: EdgeInsets.all(30.w),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(13.w),
+          color: currentTheme.accentColor.withOpacity(0.125),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            SvgPicture.asset(
+              R.ASSETS_PLACEHOLDERS_NO_NETWORK_SVG,
+              width: 50.w,
+              color: currentTheme.iconTheme.color,
             ),
-          ),
-          VGap(10.w),
-          Text(
-            details.exception.toString(),
-            style: TextStyle(
-              color: currentTheme.textTheme.caption.color,
-              fontSize: 20.sp,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          VGap(10.w),
-          Text(
-            details.stack.toString(),
-            style: TextStyle(
-              color: currentTheme.textTheme.caption.color,
-              fontSize: 16.sp,
-            ),
-            maxLines: 14,
-            overflow: TextOverflow.ellipsis,
-          ),
-          VGap(20.w),
-          Tapper(
-            onTap: _takeAppScreenshot,
-            child: Container(
-              padding: EdgeInsets.all(16.w),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(13.w),
-                color: currentTheme.accentColor,
+            VGap(20.w),
+            Text(
+              '出现了不可预料的错误 (>_<)',
+              style: TextStyle(
+                color: currentTheme.textTheme.caption.color,
+                fontSize: 22.sp,
               ),
-              child: Text(
-                '保存当前位置错误截图',
-                style: TextStyle(
-                  color: adaptiveButtonColor(),
-                  fontSize: 20.sp,
-                  height: 1.24,
-                  fontWeight: FontWeight.bold,
+            ),
+            VGap(10.w),
+            Text(
+              details.exception.toString(),
+              style: TextStyle(
+                color: currentTheme.textTheme.caption.color,
+                fontSize: 20.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            VGap(10.w),
+            Text(
+              details.stack.toString(),
+              style: TextStyle(
+                color: currentTheme.textTheme.caption.color,
+                fontSize: 16.sp,
+              ),
+              maxLines: 14,
+              overflow: TextOverflow.ellipsis,
+            ),
+            VGap(20.w),
+            Tapper(
+              onTap: _takeAppScreenshot,
+              child: Container(
+                padding: EdgeInsets.all(16.w),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(13.w),
+                  color: currentTheme.accentColor,
+                ),
+                child: Text(
+                  '保存当前位置错误截图',
+                  style: TextStyle(
+                    color: adaptiveButtonColor(),
+                    fontSize: 20.sp,
+                    height: 1.24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   };
